db4a6abd0ebd1e3473341271defb46274fb9fc6f
subhadeepchowdhury41 <subhadeepchowdhury41@gmail.com>
2022-08-22 20:42:51 +0530

    initial commit

---
+++ veridocs/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Form(models.Model):
+    name = models.TextField(max_length=50)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    data = models.JSONField()
+    createdBy = models.TextField(max_length=30)
