ae0a37a56152273884979b3feb51c929f8c367a8
subhadeepchowdhury41 <subhadeepchowdhury41@gmail.com>
2022-08-24 20:24:05 +0530

    changes

--- veridocs/api/models.py
+++ veridocs/api/models.py
@@ -6,3 +6,6 @@ class Form(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     data = models.JSONField()
     createdBy = models.TextField(max_length=30)
+
+    def __str__(self):
+        return self.name
